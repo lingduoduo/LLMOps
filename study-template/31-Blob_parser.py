68687ada62c4e35addf813a58cf3d9c63eee4ba5
Ling <linghypshen@gmail.com>
2025-04-19 23:20:33 -0400

    Data Loader

--- study-template/31-Blob_parser.py
+++ study-template/31-Blob_parser.py
@@ -36,3 +36,23 @@ documents = list(parser.lazy_parse(blob))
 print(documents)
 print(len(documents))
 print(documents[0].metadata)
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : linghypshen@gmail.com
+@File    : 2.FileSystemBlobLoader示例.py
+"""
+# from langchain_community.document_loaders.blob_loaders import FileSystemBlobLoader
+#
+# loader = FileSystemBlobLoader("./meow_meow.txt", show_progress=True)
+#
+# for blob in loader.yield_blobs():
+#     print(blob.as_string())
+
+from langchain_community.document_loaders.generic import GenericLoader
+
+loader = GenericLoader.from_filesystem(".", glob="*.txt", show_progress=True)
+
+for idx, doc in enumerate(loader.lazy_load()):
+    print(f"Loading file #{idx}, filename: {doc.metadata['source']}")
