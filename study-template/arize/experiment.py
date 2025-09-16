12642595e1eb49e9babd91622e4d24d66558ebfa
linghypshen@gmail.com <linghypshen@gmail.com>
2025-09-16 15:01:21 -0400

    add data

--- study-template/arize/experiment.py
+++ study-template/arize/experiment.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import dotenv
-import os
 import datetime
+import os
 from uuid import uuid1
 
+import dotenv
+import pandas as pd
 from arize.experimental.datasets import ArizeDatasetsClient
 from arize.experimental.datasets.utils.constants import GENERATIVE
-
-import pandas as pd
 
 dotenv.load_dotenv()
 
 SPACE_ID = os.getenv("ARIZE_SPACE_ID")
 API_KEY = os.getenv("ARIZE_API_KEY")
-print(API_KEY)
 
-os.environ['NO_PROXY'] = "otlp.arize.com" or "flight.arize.com"
+# os.environ['NO_PROXY'] = "otlp.arize.com" or "flight.arize.com"
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("/Users/linghuang/Git/LLMOps/study-template/arize/data.csv")
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 arize_client = ArizeDatasetsClient(api_key=API_KEY)
 # Create a dataset from a DataFrame add your own data here
 dataset_name = "docs-qa-new-" + str(uuid1())[:5]
